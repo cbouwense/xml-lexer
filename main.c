e870f6649a2441b7ffd7fef36a84342b9d261f51
Christian Bouwense <cbouwense@proton.me>
2025-08-13 21:48:14 -0400

    think I got it

--- main.c
+++ main.c
@@ -4,16 +4,19 @@
 #define A (GupAllocator*)&a
 
 typedef enum {
+    STATE_DEFAULT = 0,
+    STATE_STRING_LITERAL,
+    STATE_TEXT_CONTENT,
+} State;
+
+typedef enum {
     TOKEN_KIND_EOF = 0,
     TOKEN_KIND_EQUAL,
     TOKEN_KIND_FORWARD_SLASH,
-    TOKEN_KIND_FORWARD_SLASH_GREATER_THAN,
     TOKEN_KIND_GREATER_THAN,
     TOKEN_KIND_IDENTIFIER,
     TOKEN_KIND_LESS_THAN,
-    TOKEN_KIND_LESS_THAN_FORWARD_SLASH,
-    TOKEN_KIND_LESS_THAN_QUESTION,
-    TOKEN_KIND_QUESTION_GREATER_THAN,
+    TOKEN_KIND_QUESTION,
     TOKEN_KIND_QUOTE_DOUBLE,
     TOKEN_KIND_QUOTE_SINGLE,
     TOKEN_KIND_STRING_LITERAL,
@@ -33,14 +36,11 @@ void token_print(const Token token) {
     switch (token.kind) {
         case TOKEN_KIND_EOF:                        printf("  kind: TOKEN_KIND_EOF\n");                        break;
         case TOKEN_KIND_EQUAL:                      printf("  kind: TOKEN_KIND_EQUAL\n");                      break;    
-        case TOKEN_KIND_FORWARD_SLASH:              printf("  kind: TOKEN_KIND_FORWARD_SLASH\n");              break;            
-        case TOKEN_KIND_FORWARD_SLASH_GREATER_THAN: printf("  kind: TOKEN_KIND_FORWARD_SLASH_GREATER_THAN\n"); break;                        
+        case TOKEN_KIND_FORWARD_SLASH:              printf("  kind: TOKEN_KIND_FORWARD_SLASH\n");              break;                      
         case TOKEN_KIND_GREATER_THAN:               printf("  kind: TOKEN_KIND_GREATER_THAN\n");               break;        
         case TOKEN_KIND_IDENTIFIER:                 printf("  kind: TOKEN_KIND_IDENTIFIER\n");                 break;        
         case TOKEN_KIND_LESS_THAN:                  printf("  kind: TOKEN_KIND_LESS_THAN\n");                  break;        
-        case TOKEN_KIND_LESS_THAN_FORWARD_SLASH:    printf("  kind: TOKEN_KIND_LESS_THAN_FORWARD_SLASH\n");    break;                    
-        case TOKEN_KIND_LESS_THAN_QUESTION:         printf("  kind: TOKEN_KIND_LESS_THAN_QUESTION\n");         break;                
-        case TOKEN_KIND_QUESTION_GREATER_THAN:      printf("  kind: TOKEN_KIND_QUESTION_GREATER_THAN\n");      break;                    
+        case TOKEN_KIND_QUESTION:                   printf("  kind: TOKEN_KIND_QUESTION\n");                   break;                    
         case TOKEN_KIND_QUOTE_DOUBLE:               printf("  kind: TOKEN_KIND_QUOTE_DOUBLE\n");               break;        
         case TOKEN_KIND_QUOTE_SINGLE:               printf("  kind: TOKEN_KIND_QUOTE_SINGLE\n");               break;        
         case TOKEN_KIND_STRING_LITERAL:             printf("  kind: TOKEN_KIND_STRING_LITERAL\n");             break;        
@@ -79,7 +79,7 @@ bool is_valid_identifier_char(const char c) {
     return is_alpha(c) || is_numeric(c) || c == '-' || c == '_' || c == ':' || c == '.';
 }
 
-void add_token(GupAllocator* a, GupArrayPtr* tokens, TokenKind token_kind, char* lexeme_text, int line) {
+void add_token(GupAllocator* a, GupArrayPtr* tokens, TokenKind token_kind, char* lexeme_text, int line, int* curr, int lexeme_length) {
     Token* token = gup_alloc(a, sizeof(Token));
 
     token->kind        = token_kind;
@@ -87,12 +87,45 @@ void add_token(GupAllocator* a, GupArrayPtr* tokens, TokenKind token_kind, char*
     token->line        = line;
 
     gup_array_ptr_append(a, tokens, token);
+
+    token_print(*token);
+
+    *curr = *curr + lexeme_length;
 }
 
-int main(int argc, char **argv) {
+void add_string_token(GupAllocator* a, GupArrayPtr* tokens, const char* xml_file_as_string, int* curr, int start, int line, TokenKind kind, char delim) {
+    char next_c = xml_file_as_string[ *curr + 1 ];
+
+    while (next_c != delim) {
+        *curr = *curr + 1;
+        next_c = xml_file_as_string[ *curr + 1 ];
+    }
+
+    //
+    // Copy the string literal into the token
+    //
+    const size_t token_string_length = *curr - start + 1;
+    char* token_string = gup_alloc(a, sizeof(char) * token_string_length);
+    gup_cstr_copy_n(token_string, xml_file_as_string + start, token_string_length);
+
+    add_token(a, tokens, kind, token_string, line, curr, 1);
+}
+
+TokenKind delim_to_token_kind(const char delim) {
+    switch (delim) {
+        case '\'': return TOKEN_KIND_QUOTE_SINGLE;
+        case '"':  return TOKEN_KIND_QUOTE_DOUBLE;
+        case '<':  return TOKEN_KIND_LESS_THAN;
+        default: {
+            fprintf(stderr, "Unexpected delim '%c' what are you doin bro", delim);
+            exit(1);
+        } break;
+    }
+}
+
+int main(void) {
     bool had_error = false;
     GupAllocatorArena a = gup_allocator_arena_create(1024 * 1024); // 1 MB
-    // GupAllocatorBucket a = gup_allocator_bucket_create();
 
     // Read the contents of the XML file into memory as a simple string
     // TODO: parameterize input file
@@ -100,146 +133,115 @@ int main(int argc, char **argv) {
     bool success = gup_file_read_as_cstr(A, "simple.xml", &xml_file_as_string);
     gup_assert_verbose(success, "Failed to read input xml file");
 
-    // Variable to keep track of the location of the cursor
+    State state = STATE_DEFAULT;
     int start = 0;
     int curr = 0;
     int line = 1;
-
-    // State
-    bool is_tokenizing_string_literal = false;
-    char string_literal_delim_kind = TOKEN_KIND_EOF;
+    // This is the thing that will signify the tokenizer that the string literal has ended. For example, if we find a
+    // double quote as our first character of a token, then we will know that we are parsing a string literal, and that
+    // once we find another double quote we have reached the end of that string literal.
+    char delim = '\0';
 
     GupArrayPtr tokens = gup_array_ptr_create(A);
     const int char_count_in_xml_file = gup_cstr_length_excluding_null(xml_file_as_string);
 
     // Each iteration of this loop poops out exactly one token.
     while (start < char_count_in_xml_file) {
-        // This is the first character of the next token we're creating.
         const char c = xml_file_as_string[start];
-        // printf("%c (%d)\n", c, c);
 
-        switch (c) {
-            case '<': {
-                const char next_c = xml_file_as_string[ start + 1 ];
+        switch (state) {
+            case STATE_STRING_LITERAL: // Intentional fallthrough
+            case STATE_TEXT_CONTENT: {
+                if (c == delim) {
+                    add_token(A, &tokens, delim_to_token_kind(delim), NULL, line, &curr, 1);
+                    
+                    state = STATE_DEFAULT;
+                    delim = '\0';
+                } else {
+                    add_string_token(A, &tokens, xml_file_as_string, &curr, start, line, TOKEN_KIND_STRING_LITERAL, delim);
+                }
+            } break;
 
-                switch (next_c) {
+            case STATE_DEFAULT: {
+                switch (c) {
+                    case '<': {
+                        add_token(A, &tokens, TOKEN_KIND_LESS_THAN, NULL, line, &curr, 1);
+                    } break;
+
                     case '?': {
-                        add_token(A, &tokens, TOKEN_KIND_LESS_THAN_QUESTION, NULL, line);
-                        curr++;
+                        add_token(A, &tokens, TOKEN_KIND_QUESTION, NULL, line, &curr, 1);
                     } break;
 
                     case '/': {
-                        add_token(A, &tokens, TOKEN_KIND_LESS_THAN_FORWARD_SLASH, NULL, line);
+                        add_token(A, &tokens, TOKEN_KIND_FORWARD_SLASH, NULL, line, &curr, 1);
+                    } break;
+
+                    case '=': {
+                        add_token(A, &tokens, TOKEN_KIND_EQUAL, NULL, line, &curr, 1);
+                    } break;
+
+                    case '\'': {
+                        add_token(A, &tokens, TOKEN_KIND_QUOTE_SINGLE, NULL, line, &curr, 1);
+                        state = STATE_STRING_LITERAL;
+                        delim = '\'';
+                    } break;
+
+                    case '"': {
+                        add_token(A, &tokens, TOKEN_KIND_QUOTE_DOUBLE, NULL, line, &curr, 1);
+                        state = STATE_STRING_LITERAL;
+                        delim = '"';
+                    } break;
+
+                    case '>': {
+                        add_token(A, &tokens, TOKEN_KIND_GREATER_THAN, NULL, line, &curr, 1);
+                        state = STATE_TEXT_CONTENT;
+                        delim = '<';
+                    } break;
+
+                    // Whitespace isn't all noise in XML, but it never starts a new token. Since this is the outer switch where
+                    // we are starting new tokens, we skip any whitespace here specifically.
+                    case ' ': {
                         curr++;
                     } break;
 
-                    default : {
-                        add_token(A, &tokens, TOKEN_KIND_LESS_THAN, NULL, line);
+                    case '\n': {
+                        line++;
+                        curr++;
                     } break;
-                }
-            } break;
-
-            case '/': {
-                const char next_c = xml_file_as_string[ start + 1 ];
-
-                if (next_c == '>') {
-                    add_token(A, &tokens, TOKEN_KIND_FORWARD_SLASH_GREATER_THAN, NULL, line);
-                    curr++;
-                } else {
-                    // TODO: I don't even think this is valid? Maybe that's for the parser to decide.
-                    add_token(A, &tokens, TOKEN_KIND_FORWARD_SLASH, NULL, line);
-                }
-            } break;
-
-            case '>': {
-                add_token(A, &tokens, TOKEN_KIND_GREATER_THAN, NULL, line);
-                is_tokenizing_string_literal = true;
-                string_literal_delim_kind = TOKEN_KIND_QUOTE_SINGLE;
-            } break;
-
-            case '?': {
-                const char next_c = xml_file_as_string[ start + 1 ];
-
-                if (next_c == '>') {
-                    add_token(A, &tokens, TOKEN_KIND_QUESTION_GREATER_THAN, NULL, line);
-                    curr++;
-                }
-            } break;
-
-            case '\n': {
-                line++;
-            } break;
-
-            case '=': {
-                add_token(A, &tokens, TOKEN_KIND_EQUAL, NULL, line);
-            } break;
-
-            case '\'': {
-                add_token(A, &tokens, TOKEN_KIND_QUOTE_SINGLE, NULL, line);
-                is_tokenizing_string_literal = !is_tokenizing_string_literal;
-                string_literal_delim_kind = TOKEN_KIND_QUOTE_SINGLE;
-            } break;
-
-            case '\"': {
-                add_token(A, &tokens, TOKEN_KIND_QUOTE_DOUBLE, NULL, line);
-                is_tokenizing_string_literal = !is_tokenizing_string_literal;
-                string_literal_delim_kind = TOKEN_KIND_QUOTE_DOUBLE;
-            } break;
-
-            // Whitespace isn't all noise in XML, but it never starts a new token. Since this is the outer switch where
-            // we are starting new tokens, we skip any whitespace here specifically.
-            case ' ': break;
-            
-            default: {
-                if (is_tokenizing_string_literal) {
-                    char next_c = xml_file_as_string[ curr + 1 ];
                     
-                    char delim = '\0';
-                    switch (string_literal_delim_kind) {
-                        case TOKEN_KIND_QUOTE_DOUBLE: delim = '"';  break;
-                        case TOKEN_KIND_QUOTE_SINGLE: delim = '\''; break;
-                        case TOKEN_KIND_GREATER_THAN: delim = '<';  break;
-                        default: {
-                            fprintf(stderr, "Encountered invalid string literal delimeter kind. This indicates a bug with the xml lexer.\n");
-                            exit(1);
-                        } break;
-                    }
+                    default: {
+                        // Identifier
+                        if (is_valid_identifier_starting_char(c)) {
+                            char next_c = xml_file_as_string[ curr + 1 ];
 
-                    while (next_c != delim) {
-                        curr++;
-                        next_c = xml_file_as_string[ curr + 1 ];
-                    }
+                            while (is_valid_identifier_char(next_c)) {
+                                curr++;
+                                next_c = xml_file_as_string[ curr + 1 ];
+                            }
 
-                    const size_t string_literal_length = (curr - start + 1); // +1 for null terminator.
-                    char* string_literal = gup_alloc(A, sizeof(char) * string_literal_length);
-                    gup_cstr_copy_n(string_literal, xml_file_as_string + start, string_literal_length);
+                            //
+                            // Copy the identifier string into the token
+                            //
+                            const size_t identifier_string_length = curr - start + 1;
+                            char* identifier_string = gup_alloc(A, sizeof(char) * identifier_string_length);
+                            gup_cstr_copy_n(identifier_string, xml_file_as_string + start, identifier_string_length);
 
-                    add_token(A, &tokens, TOKEN_KIND_STRING_LITERAL, string_literal, line);
-                    curr += string_literal_length - 2;
-                } else if (is_valid_identifier_starting_char(c)) {
-                    // Scan over the identifier while it's valid
-                    char next_c = xml_file_as_string[curr + 1];
-                    while (is_valid_identifier_char(next_c)) {
-                        curr++;
-                        next_c = xml_file_as_string[curr + 1];
-                    }
-
-                    // Allocate the lexeme string
-                    const size_t lexeme_length = curr - start + 1; // plus 1 null terminator
-                    char* lexeme = gup_alloc(A, sizeof(char) * lexeme_length);
-
-                    // Copy the string into lexeme.
-                    gup_cstr_copy_n(lexeme, xml_file_as_string + start, lexeme_length);
-
-                    add_token(A, &tokens, TOKEN_KIND_IDENTIFIER, lexeme, line);
-                } else {
-                    had_error = true;
-                    fprintf(stderr, "Unexpected character `%c` at line %d\n", c, line);
+                            add_token(A, &tokens, TOKEN_KIND_IDENTIFIER, identifier_string, line, &curr, 1);
+                        } else {
+                            had_error = true;
+                            fprintf(stderr, "Unexpected character `%c` at line %d\n", c, line);
+                            exit(1); // TODO: remove this for actual thing
+                        }
+                    } break;
                 }
             } break;
+
+            default: {
+                fprintf(stderr, "Unknown tokenizer state");
+                exit(1);
+            }
         }
 
-        curr++;
         start = curr;
     }
 
