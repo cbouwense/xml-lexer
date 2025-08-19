5e21fd91e3bf8697c74e1e8208f036ef927f43d0
Christian Bouwense <cbouwense@proton.me>
2025-08-18 22:26:49 -0400

    added types for different xml expressions

---
+++ xml_parser.h
@@ -0,0 +1,46 @@
+typedef struct {
+    char* name;
+    char* value;
+} GupXmlExprAttribute;
+
+typedef struct {
+    GupXmlExprElement element;
+    char* char_data;
+} GupXmlExprContent;
+
+typedef struct {
+    char* name;
+} GupXmlExprEndTag;
+
+typedef struct {
+    char* name;
+    GupXmlExprAttribute attribute;
+} GupXmlExprStartTag;
+
+typedef GupXmlExprStartTag GupXmlExprEmptyElementTag;
+
+typedef struct {
+    GupXmlExprStartTag start_tag;
+    GupXmlExprContent content;
+    GupXmlExprEndTag end_tag;
+    GupXmlExprEmptyElementTag empty_element_tag;
+} GupXmlExprElement;
+
+typedef struct {
+    char* name;
+    GupXmlExprExternalId external_id;
+    GupXmlExprInternalSubset internal_subset;
+} GupXmlExprDoctypeDeclaration;
+
+typedef struct {
+    GupXmlExprAttribute attribute;
+} GupXmlExprXmlDeclaration;
+
+typedef struct {
+    GupXmlExprXmlDeclaration xml_declaration;
+} GupXmlExprProlog;
+
+typedef struct {
+    GupXmlExprProlog prolog;
+    GupXmlExprElement element;
+} GupXmlExprDocument;
